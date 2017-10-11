db36f84936643af5b0d7e2ed2d04af3a83bdea20
s940503g <s940503g@hotmail.com>
2017-10-11 18:21:50 +0800

    Better modulized js.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const events = require('events');
-const child_prcess = require('child_process');
-const parser = require('xml2json').toJSON;
+const child_process = require('child_process');
+const parser = require('xml2json').toJson;
 const isReachable = require('is-reachable');
 
 /*
@@ -9,27 +9,31 @@ const isReachable = require('is-reachable');
  * poll the node state
  * control nodes / scene
  */
+// var Zwave = {};
 
-module.exports = Zwave = {};
+function Zwave () {}
 
-Zwave.createConnection = function (setting, callback) {
-	this.setting = setting;
-	let path = 'http://' + setting.ip + ':5000';
+exports.createConnection = function (setting) {
+	let zwave = new Zwave();
+	zwave.setting = setting;
+	return zwave;
+}
+
+Zwave.prototype.connect = function (callback) {
+	let path = this.setting.ip + ':5000';
 	isReachable(path).then((reachable) => {
 		var err = null;
 		if (!reachable) err = new Error("Can't create connection to " + path + ' . Please check the network of the gateway.');
 		callback(err);
 	});
-
-	return this;
 }
 
-Zwave.poll = function (node, callback) {
+Zwave.prototype.pollNode = function (node, callback) {
 	let poll = (nodeId) => {
-		let cmd = 'curl --digest -u ' + this.setting.acc + ':' + this.setting.password + ' ' + this.setting.ip + ':5000/node_detail.cgi -d "fun=load&id=' + nodeId + '"';
+		let cmd = 'curl --digest -u ' + this.setting.acc + ':' + this.setting.pwd + ' ' + this.setting.ip + ':5000/node_detail.cgi -d "fun=load&id=' + nodeId + '"';
 		child_process.exec(cmd, function (err, stdout) {
 			let json = parser(stdout, {object: true});
-			callback(err, json);
+			callback(err, json.node_detail.node);
 		});
 	}
 
@@ -42,14 +46,23 @@ Zwave.poll = function (node, callback) {
 	}
 }
 
-/* EXAMPLE */
+Zwave.prototype.pollAllNodeDetail = function (callback) {
+	let cmd = 'curl --digest -u ' + this.setting.acc + ':' + this.setting.pwd + ' ' + this.setting.ip + ':5000/node_detail.cgi -d "fun=load"';
 
-const zwave = require('zwave');
+	console.log(cmd);
 
-let myGateway = new zwave.createConnection({
-	acc:'admin',
-	pwd: '123456',
-	ip: '10.10.1.251'
-}, function (err) {
-	console.log(err);
-});
+	child_process.exec(cmd, function (err, stdout) {
+		let json = parser(stdout, {object: true});
+		callback(err, json.node_detail);
+	});
+}
+
+Zwave.prototype.control = function (value, callback) {
+	let para = value.nodeId + '-' + value.class.replace(/\s/g, '+') + '-' + value.type + '-' + value.instance + '-' + value.index + '=' + value.input;
+	let cmd = 'curl --digest -u ' + this.setting.acc + ':' + this.setting.pwd + ' ' + this.setting.ip + ':5000/valuepost.html -d "' + para + '"';
+
+	child_process.exec(cmd, function (err, stdout) {
+		console.log(stdout);
+		callback(err);
+	});
+}
